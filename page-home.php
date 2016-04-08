8c5cdb6cda058f44cb907c5222b66a9c858681c2
ReinKrijgsman <reinkrijgsman@gmail.com>
2016-04-08 10:51:17 +0200

    Added simple WP Loop

--- page-home.php
+++ page-home.php
@@ -2,6 +2,14 @@
 
 <?php get_header(); ?>
 
-<h1>This is the homepage</h1>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <h1><?php the_title(); ?></h1>
+  <p><?php the_content(); ?></p>
+
+<?php endwhile; else : ?>
+  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
-<h1>This is a page</h1>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <h1><?php the_title(); ?></h1>
+  <p><?php the_content(); ?></p>
+
+<?php endwhile; else : ?>
+  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+
 
 <?php get_footer(); ?>
